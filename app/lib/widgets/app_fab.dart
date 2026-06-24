c1cf07f9eab7f27b7a2e82cb6bcadc0579deeedc
popchami <47472197+popchami@users.noreply.github.com>
2026-06-24 21:25:11 +0900

    Rename FAB copy for KickxKick

--- app/lib/widgets/app_fab.dart
+++ app/lib/widgets/app_fab.dart
@@ -8,10 +8,10 @@ class AppFab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton.extended(
-      tooltip: 'スニーカーを収蔵する',
-      icon: const Icon(Icons.inventory_2_outlined),
+      tooltip: 'スニーカーを追加',
+      icon: const Icon(Icons.add),
       label: const Text(
-        '収蔵する',
+        '追加',
         style: TextStyle(
           fontWeight: FontWeight.w700,
           letterSpacing: 0.6,
