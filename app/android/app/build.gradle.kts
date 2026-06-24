d637c75fd70e197ed0686b196d9e5b6f85320b5a
popchami <47472197+popchami@users.noreply.github.com>
2026-06-24 16:48:03 +0900

    Rename Android application id to KickxKick

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.popchami.solemuseum"
+    namespace = "com.popchami.kickxkick"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,10 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.popchami.solemuseum"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.popchami.kickxkick"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
