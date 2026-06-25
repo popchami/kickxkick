5ba46c51d3252fcfd4dd5975f55eeb222a648251
popchami <47472197+popchami@users.noreply.github.com>
2026-06-25 15:29:28 +0900

    Export search repository and providers

--- app/lib/features/search/search.dart
+++ app/lib/features/search/search.dart
@@ -1,4 +1,6 @@
 export 'search_index.dart';
 export 'search_models.dart';
 export 'search_normalizer.dart';
+export 'search_providers.dart';
+export 'search_repository.dart';
 export 'search_service.dart';
